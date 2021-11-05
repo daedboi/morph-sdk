746848ffdeaad01a5a3bc07e19c9582edad47da9
daedboi <87483308+daedboi@users.noreply.github.com>
2021-11-05 23:44:04 +0200

    init

---
+++ src/constants.ts
@@ -0,0 +1,47 @@
+import JSBI from 'jsbi'
+
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
+
+export enum ChainId {
+  MAINNET = 250,
+  BSCTESTNET = 4002
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+export const FACTORY_ADDRESS = '0x9C454510848906FDDc846607E4baa27Ca999FBB6'
+
+export const INIT_CODE_HASH = '0x82b04e3f5f54946a0ed70d459c25f54ad42d498268c75b27f9727e3d212e91fd'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
+export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
+export const _997 = JSBI.BigInt(9985)
+export const _1000 = JSBI.BigInt(10000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
---
+++ test/pair.test.ts
@@ -0,0 +1,118 @@
+import { ChainId, Token, Pair, TokenAmount, WETH, Price } from '../src'
+
+describe('Pair', () => {
+  const DAI = new Token(ChainId.MAINNET, '0x04068da6c83afcfa0e13ba15a6696662335d5b75', 6, 'USDC', 'USD Coin')
+  const USDC  = new Token(ChainId.MAINNET, '0x8d11ec38a3eb5e956b052f67da8bdc9bef8abf3e', 18, 'DAI', 'DAI Stablecoin')
+
+  describe('constructor', () => {
+    it('cannot be used for tokens on different chains', () => {
+      expect(() => new Pair(new TokenAmount(USDC, '100'), new TokenAmount(WETH[ChainId.BSCTESTNET], '100'))).toThrow(
+        'CHAIN_IDS'
+      )
+    })
+  })
+
+  describe('#getAddress', () => {
+    it('returns the correct address', () => {
+      expect(Pair.getAddress(USDC, DAI)).toEqual('0x9606D683d03f012DDa296eF0ae9261207C4A5847')
+    })
+  })
+
+  describe('#token0', () => {
+    it('always is the token that sorts before', () => {
+      expect(new Pair(new TokenAmount(USDC, '100'), new TokenAmount(DAI, '100')).token0).toEqual(DAI)
+      expect(new Pair(new TokenAmount(DAI, '100'), new TokenAmount(USDC, '100')).token0).toEqual(DAI)
+    })
+  })
+  describe('#token1', () => {
+    it('always is the token that sorts after', () => {
+      expect(new Pair(new TokenAmount(USDC, '100'), new TokenAmount(DAI, '100')).token1).toEqual(USDC)
+      expect(new Pair(new TokenAmount(DAI, '100'), new TokenAmount(USDC, '100')).token1).toEqual(USDC)
+    })
+  })
+  describe('#reserve0', () => {
+    it('always comes from the token that sorts before', () => {
+      expect(new Pair(new TokenAmount(USDC, '100'), new TokenAmount(DAI, '101')).reserve0).toEqual(
+        new TokenAmount(DAI, '101')
+      )
+      expect(new Pair(new TokenAmount(DAI, '101'), new TokenAmount(USDC, '100')).reserve0).toEqual(
+        new TokenAmount(DAI, '101')
+      )
+    })
+  })
+  describe('#reserve1', () => {
+    it('always comes from the token that sorts after', () => {
+      expect(new Pair(new TokenAmount(USDC, '100'), new TokenAmount(DAI, '101')).reserve1).toEqual(
+        new TokenAmount(USDC, '100')
+      )
+      expect(new Pair(new TokenAmount(DAI, '101'), new TokenAmount(USDC, '100')).reserve1).toEqual(
+        new TokenAmount(USDC, '100')
+      )
+    })
+  })
+
+  describe('#token0Price', () => {
+    it('returns price of token0 in terms of token1', () => {
+      expect(new Pair(new TokenAmount(USDC, '101'), new TokenAmount(DAI, '100')).token0Price).toEqual(
+        new Price(DAI, USDC, '100', '101')
+      )
+      expect(new Pair(new TokenAmount(DAI, '100'), new TokenAmount(USDC, '101')).token0Price).toEqual(
+        new Price(DAI, USDC, '100', '101')
+      )
+    })
+  })
+
+  describe('#token1Price', () => {
+    it('returns price of token1 in terms of token0', () => {
+      expect(new Pair(new TokenAmount(USDC, '101'), new TokenAmount(DAI, '100')).token1Price).toEqual(
+        new Price(USDC, DAI, '101', '100')
+      )
+      expect(new Pair(new TokenAmount(DAI, '100'), new TokenAmount(USDC, '101')).token1Price).toEqual(
+        new Price(USDC, DAI, '101', '100')
+      )
+    })
+  })
+
+  describe('#priceOf', () => {
+    const pair = new Pair(new TokenAmount(USDC, '101'), new TokenAmount(DAI, '100'))
+    it('returns price of token in terms of other token', () => {
+      expect(pair.priceOf(DAI)).toEqual(pair.token0Price)
+      expect(pair.priceOf(USDC)).toEqual(pair.token1Price)
+    })
+
+    it('throws if invalid token', () => {
+      expect(() => pair.priceOf(WETH[ChainId.MAINNET])).toThrow('TOKEN')
+    })
+  })
+
+  describe('#reserveOf', () => {
+    it('returns reserves of the given token', () => {
+      expect(new Pair(new TokenAmount(USDC, '100'), new TokenAmount(DAI, '101')).reserveOf(USDC)).toEqual(
+        new TokenAmount(USDC, '100')
+      )
+      expect(new Pair(new TokenAmount(DAI, '101'), new TokenAmount(USDC, '100')).reserveOf(USDC)).toEqual(
+        new TokenAmount(USDC, '100')
+      )
+    })
+
+    it('throws if not in the pair', () => {
+      expect(() =>
+        new Pair(new TokenAmount(DAI, '101'), new TokenAmount(USDC, '100')).reserveOf(WETH[ChainId.MAINNET])
+      ).toThrow('TOKEN')
+    })
+  })
+
+  describe('#chainId', () => {
+    it('returns the token0 chainId', () => {
+      expect(new Pair(new TokenAmount(USDC, '100'), new TokenAmount(DAI, '100')).chainId).toEqual(ChainId.MAINNET)
+      expect(new Pair(new TokenAmount(DAI, '100'), new TokenAmount(USDC, '100')).chainId).toEqual(ChainId.MAINNET)
+    })
+  })
+  describe('#involvesToken', () => {
+    expect(new Pair(new TokenAmount(USDC, '100'), new TokenAmount(DAI, '100')).involvesToken(USDC)).toEqual(true)
+    expect(new Pair(new TokenAmount(USDC, '100'), new TokenAmount(DAI, '100')).involvesToken(DAI)).toEqual(true)
+    expect(
+      new Pair(new TokenAmount(USDC, '100'), new TokenAmount(DAI, '100')).involvesToken(WETH[ChainId.MAINNET])
+    ).toEqual(false)
+  })
+})
